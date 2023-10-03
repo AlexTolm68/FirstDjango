25e630177130fb4fc654ee761718d1613d4fd06f
Alexander <av_tolm@mail.ru>
2023-10-03 21:13:21 +0300

    create /about

--- MainApp/views.py
+++ MainApp/views.py
@@ -8,3 +8,14 @@ def home(request):
     text = """< h1 > "Изучаем django" < / h1 >
     < strong > Автор < / strong >: < i > Иванов < / i > """
     return HttpResponse(text)
+
+
+def about(request):
+    firstname = "Александр"
+    secondname = "Толмачев"
+    otchestvo =  "Владимирович"
+    phone = "8-923-600-01-02"
+    email = "av_tolm@mail.ru"
+
+    text = f"Имя: {firstname}\nОтчество: {otchestvo}\nФамилия: {secondname}\nТелефон: {secondname}\nemail: {secondname}"
+    return HttpResponse(text)
