245e975a8fa48ba50eff119ddff667c8bfd552ae
Alexander <av_tolm@mail.ru>
2023-10-04 15:51:22 +0300

    практика: простая обработка запросов

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def home(request):
-    text = """< h1 > "Изучаем django" < / h1 >
-    < strong > Автор < / strong >: < i > Иванов < / i > """
+    text = f"""<h1> "Изучаем django" </h1>
+    <strong> Автор </strong >: <i> Шиховцев Вадим Викторович </i>"""
     return HttpResponse(text)
 
 
 def about(request):
     firstname = "Александр"
     secondname = "Толмачев"
-    otchestvo =  "Владимирович"
+    otchestvo = "Владимирович"
     phone = "8-923-600-01-02"
     email = "av_tolm@mail.ru"
 
-    text = f"""<h1>Имя: {firstname} Отчество: {otchestvo} Фамилия: {secondname} Телефон: {phone} email: {email}</h1>"""
+    text = f"""<h>Имя: {firstname}, Отчество: {otchestvo}, Фамилия: {secondname}, Телефон: {phone}, email: {email}</h>"""
     return HttpResponse(text)
+
 
 items = [
     {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
@@ -28,8 +30,28 @@ items = [
     {"id": 8, "name": "Кепка", "quantity": 124}
 ]
 
-def item(request):
 
-    return HttpResponse()
+def get_items(request, id_item):
+    try:
+        for item in items:
+            if item["id"] == id_item:
+                name = item["name"]
+                quantity = item["quantity"]
+                break
+        text = f"""<h> id: {id_item} name: {name} quantity: {quantity}</h>"""
+    except UnboundLocalError:
+        text = f"Товар с id={id_item} не найден"
+
+    return HttpResponse(text)
 
 
+def list_items(request):
+    text = ""
+    for item in items:
+        id_item = item["id"]
+        name = item["name"]
+        quantity = item["quantity"]
+        goods = f"""<h> id: {id_item} name: {name} quantity: {quantity}</h> <a href="/items/{id_item}">id:{id_item}</a>,  
+        \n"""
+        text += goods
+    return HttpResponse(text)
